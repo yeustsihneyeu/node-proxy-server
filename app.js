ddeda5cb3825c17b7f49f02a2587e6c0f542deb5
ayeustsihneyeu <ayeustsihneyeu@piesoft.us>
2024-04-02 16:42:54 +0200

    PROXY-2: Make first server to server API request

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const axios = require('axios');
+const sprintf = require('sprintf-js').sprintf;
+
+
+// PROXY-1: Print Hello World
 console.log("Hello world!");
+
+// PROXY-2: Make first server to server API request
+const START_DATE = "2024-04-01";
+const END_DATE = "2024-04-05";
+const API_KEY = "";
+const URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=%s&end_date=%s&api_key=%s";
+
+axios
+    .get(sprintf(URL, START_DATE, END_DATE, API_KEY))
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
