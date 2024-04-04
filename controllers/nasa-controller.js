72ed5aa7331fa89f36b6f0a4d40bf4b6bc42d454
ayeustsihneyeu <a.yeustsihneyeu@godeltech.com>
2024-04-04 22:47:12 +0200

    PROXY-7: Add catch(next)

--- controllers/nasa-controller.js
+++ controllers/nasa-controller.js
@@ -1,14 +1,9 @@
 import { getAsteroids } from '../client/nasa-client.js';
 import { mapToAsteroids } from '../helpers/asteroid-mapper.js'
 
-const handleError = (res, error) => {
-    console.log(error);
-    res.status(404).send(error.message);
-};
-
-export const getAsteroidsData = (req, res) => {
+export const getAsteroidsData = (req, res, next) => {
     getAsteroids(req.query.start_date, req.query.end_date)
         .then(({ data }) => mapToAsteroids(data))
         .then(asteroids => res.status(200).send(asteroids))
-        .catch(err => handleError(res, err));
+        .catch(err => next(err));
 };
