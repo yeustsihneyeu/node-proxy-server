636c3cfca292e7dd6bb4387c3de4baac292a3ef3
ayeustsihneyeu <a.yeustsihneyeu@godeltech.com>
2024-04-08 18:24:36 +0200

    PROXY-9: Add parameters to the query

---
+++ services/asteroid-service.js
@@ -0,0 +1,26 @@
+import { Asteroid } from '../models/Asteroid.js';
+import { config } from 'dotenv';
+config();
+
+const getAsteroids = (data) => {
+    const dates = data['near_earth_objects'];
+    let wereDangerousMeteors;
+    const asteroidsData = Object.keys(dates)
+        .map(key => dates[key])
+        .flat()
+        .map(el => {
+            if (el['is_potentially_hazardous_asteroid'] === true)
+                wereDangerousMeteors = true;
+            return new Asteroid(el);
+        });
+    return { asteroidsData, wereDangerousMeteors };
+}
+
+export const mapToResponse = (data, params) => {
+    const { asteroidsData, wereDangerousMeteors } = getAsteroids(data);
+    return  {
+        ...( params.count ? { count: data['element_count'] } : {} ),
+        ...( params.wereDangerousMeteors ? { wereDangerousMeteors: wereDangerousMeteors } : {} ),
+        asteroids: asteroidsData,
+    }
+}
